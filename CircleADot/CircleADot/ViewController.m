3120faa2d74dba88441f0cf146f03f7d5da3948e
landony <magicylwnn@gmail.com>
2017-10-16 14:25:00 -0700

    Update ViewController.m

--- CircleADot/CircleADot/ViewController.m
+++ CircleADot/CircleADot/ViewController.m
@@ -2,8 +2,8 @@
 //  ViewController.m
 //  CircleADot
 //
-//  Created by leweny on 4/29/16.
-//  Copyright © 2016 leweny. All rights reserved.
+//  Created by landony on 4/29/16.
+//  Copyright © 2016 landony. All rights reserved.
 //
 
 #import "ViewController.h"
