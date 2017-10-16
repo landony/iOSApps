99af9222d366b12f994f6080860399991920663a
landony <magicylwnn@gmail.com>
2017-10-16 14:27:38 -0700

    Update AppDelegate.m

--- CircleADot/CircleADot/AppDelegate.m
+++ CircleADot/CircleADot/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  CircleADot
 //
-//  Created by leweny on 4/29/16.
-//  Copyright © 2016 leweny. All rights reserved.
+//  Created by landony on 4/29/16.
+//  Copyright © 2016 landony. All rights reserved.
 //
 
 #import "AppDelegate.h"
