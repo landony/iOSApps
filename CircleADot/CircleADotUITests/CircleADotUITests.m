a02c1ec6d2a3a97eb69df199e6fdf2df66b69b90
landony <magicylwnn@gmail.com>
2017-10-16 14:32:09 -0700

    Update CircleADotUITests.m

--- CircleADot/CircleADotUITests/CircleADotUITests.m
+++ CircleADot/CircleADotUITests/CircleADotUITests.m
@@ -2,8 +2,8 @@
 //  CircleADotUITests.m
 //  CircleADotUITests
 //
-//  Created by leweny on 4/29/16.
-//  Copyright © 2016 leweny. All rights reserved.
+//  Created by landony on 4/29/16.
+//  Copyright © 2016 landony. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
